9120449f2bdd1802dbbcbb3a4a0a7de0b7f82d71
hemapriya2001 <hemapriya@twilightsoftwares.com>
2021-11-09 20:48:27 +0530

    FormArrays

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'forms';
+  name="Form array";
+  formArray: FormGroup|any ;
+  
+
+constructor 
+( private FormGroup:FormBuilder
+  )
+  {
+  this.formArray = FormGroup.group({
+    'user': FormGroup.array([])
+  })
 }
+addButton(){
+  const add = this.formArray.get('user') as FormArray;
+  const a = this.FormGroup.group({
+    skills:[]
+  }
+  )
+   add.push(a);
+   console.log(this.formArray.value);
+}
+
+removeButton(index:number){
+  const add = this.formArray.get('user') as FormArray;
+  add.removeAt(index)
+}
+}
+
+
+
+
+
+
+  

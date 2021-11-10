baa47d08e941a5c64c2c0110dd20fc471d2dbdb4
hemapriya2001 <hemapriya@twilightsoftwares.com>
2021-11-10 13:23:11 +0530

    fix: FormArrays

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
@@ -8,37 +9,57 @@ import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 })
 export class AppComponent {
   title = 'forms';
-  name="Form array";
-  formArray: FormGroup|any ;
-  
+  name = "Form array";
+  formArray: FormGroup | any;
 
-constructor 
-( private FormGroup:FormBuilder
-  )
-  {
-  this.formArray = FormGroup.group({
-    'user': FormGroup.array([])
-  })
-}
-addButton(){
-  const add = this.formArray.get('user') as FormArray;
-  const a = this.FormGroup.group({
-    skills:[]
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.formArray = this.fb.group({
+      user: this.fb.array([])
+    })
   }
-  )
-   add.push(a);
-   console.log(this.formArray.value);
+  user(): FormArray {
+    return this.formArray.get('user') as FormArray
+  }
+  addButton() {
+    const add = this.formArray.get('user') as FormArray;
+    const a = this.fb.group({
+      skills:this.fb.array([])
+    }
+    )
+    add.push(a);
+    console.log(this.formArray.value);
+  }
+
+  removeButton(index: number) {
+    const add = this.formArray.get('user') as FormArray;
+    add.removeAt(index)
+  }
+  newSkills(i: number): FormArray {
+    return this.user()
+      .at(i)
+      .get('skills') as FormArray
+  }
+  aSkill(): FormGroup {
+    return this.fb.group({
+      skill: ''
+    });
+  }
+  addSkill(i: number) {
+    console.log('i',i)
+    this.newSkills(i).push(this.aSkill());
+  }
+  removeSkill(i: number, s: number) {
+    this.newSkills(i).removeAt(s);
+  }
 }
 
-removeButton(index:number){
-  const add = this.formArray.get('user') as FormArray;
-  add.removeAt(index)
-}
-}
 
 
 
 
 
 
-  
+
+

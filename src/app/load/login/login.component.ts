eabf28fb4cd7365ce659df8f3146fb345f89f73f
hemapriya2001 <hemapriya@twilightsoftwares.com>
2021-11-17 20:24:11 +0530

    Fix: Crud-forms

--- src/app/load/login/login.component.ts
+++ src/app/load/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import {FormBuilder,FormGroup} from '@angular/forms';
+import {FormBuilder,FormGroup, Validators} from '@angular/forms';
+import { ApiService } from 'src/app/api.service';
+import { EmployeeModel } from './login.model';
 
 @Component({
   selector: 'app-login',
@@ -10,21 +12,37 @@ import {FormBuilder,FormGroup} from '@angular/forms';
 export class LoginComponent implements OnInit {
   name = 'Angular-CRUD';
   eDetails : FormGroup | any ;
-  constructor( private formbuilder:FormBuilder, private modalService: NgbModal) { }
+  submitted = false;
+  employeeModelObj:EmployeeModel = new EmployeeModel();
+  constructor( private formbuilder:FormBuilder, private modalService: NgbModal, private api:ApiService) { }
 
   ngOnInit(): void {
-    this.eDetails = this.formbuilder.group({
-      eID :[''],
-      firstName :[''],
-      lastName :[''],
-      emailId:[''],
-      mobileNo:[''],
-      salary:['']
-    })
+    
+    
+    this.createForm();
+    console.log('value',this.eDetails.value);
   }
+ 
   addEmployees(content:any){
     this.modalService.open(content);
   }
+  createForm(){
+    this.eDetails = this.formbuilder.group({
+      eId :['',Validators.required],
+      firstName :['',Validators.required],
+      lastName :['',Validators.required],
+      emailId:['',[Validators.required,Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+      mobileNo:['',Validators.required,Validators.minLength(10)],
+      salary:['',Validators.required]
+    })
+  }
   
-
+ get e(){
+   return this.eDetails.controls;
+ }
+ onSubmit(){
+  this.submitted = true;
+  console.log('form');
+  console.log(this.eDetails);
+ }
 }
---
+++ src/app/load/login/login.model.ts
@@ -0,0 +1,9 @@
+export class EmployeeModel{
+    // eId:number = 0;
+    // firstName:string ='';
+    // lastName :string ='';
+    // emailId:string ='';
+    // mobileNo:string ='';
+    // salary:string ='';
+
+}
